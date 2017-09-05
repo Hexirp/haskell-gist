6eafaf0e844911602682b0ffb9506c090418709a
Hexirp <Hexirp@users.noreply.github.com>
2017-09-05 22:49:20 +0900

    Update

--- ExtensibleIO.hs
+++ ExtensibleIO.hs
@@ -13,9 +13,16 @@ module ExtensibleIO where
  import Control.Arrow ((>>>))
  import Control.Monad.Skeleton
  import Data.Extensible
+ import Data.Proxy
 
  -- | Run a subset of 'IO' by handles.
  runIOSH :: RecordOf (Interpreter IO) xs -> Eff xs a -> IO a
  runIOSH handles = handleEff handles >>> \case
   Return a -> return a
   x :>>= f -> x >>= runIOSH handles . f
+
+ class IOSubset kv where
+  runIOS :: Proxy kv -> Interpreter IO (AssocValue kv)
+
+ getHandles :: Forall IOSubset xs => RecordOf (Interpreter IO) xs
+ getHandles = htabulateFor (Proxy :: Proxy IOSubset) _
