d45e4c17e18fe75cd92c2a4b57619a8c90618c76
Hexirp <Hexirp@users.noreply.github.com>
2017-09-05 21:21:13 +0900

    Update

--- ExtensibleIO.hs
+++ ExtensibleIO.hs
@@ -1,8 +1,18 @@
+#!/usr/bin/env stack
+{- stack
+ --resolver lts-9.2
+ --install-ghc
+ exec ghci
+ --package base
+ --package extensible
+ --package monad-skeleton
+-}
+{-# LANGUAGE LambdaCase #-}
 module ExtensibleIO where
  import Prelude
+ import Control.Arrow ((>>>))
  import Control.Monad.Skeleton
  import Data.Extensible
- import Data.Proxy
 
  -- | Run a subset of 'IO' by handles.
  runIOSH :: RecordOf (Interpreter IO) xs -> Eff xs a -> IO a
