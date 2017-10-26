b5e1a74ca27b2318d132e9976c64ef14355acfb0
Hexirp <Hexirp@users.noreply.github.com>
2017-10-26 20:47:24 +0900

    Add PatternSynonym.hs

---
+++ PatternSynonym.hs
@@ -0,0 +1,19 @@
+#!/usr/bin/env stack
+-- stack --resolver lts-9.10 --install-ghc ghci
+
+{-# LANGUAGE PatternSynonyms #-}
+
+data SExpr = Cons SExpr SExpr | AtomInt Int
+
+newtype CallowSExpr = CallowSExpr { growup :: SExpr }
+
+pattern AtomInt' :: Int -> CallowSExpr
+pattern AtomInt' x = CallowSExpr (AtomInt x)
+
+pattern Cons' :: SExpr -> SExpr -> CallowSExpr
+pattern Cons' x y = CallowSExpr (Cons x y)
+
+main :: IO ()
+main = do
+  let x = Cons' (AtomInt 1) (AtomInt 2)
+  return ()
