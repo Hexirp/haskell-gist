00bdf63f18ff7f838b9f8022489a1f69d26654af
Hexirp <Hexirp@users.noreply.github.com>
2018-04-05 17:17:26 +0900

    Update Game.hs to research about buffering mode

--- Game.hs
+++ Game.hs
@@ -3,9 +3,14 @@
 
 module Game where
   import Prelude
+  import System.IO (stdin, stdout, BufferMode(..), hGetBuffering) 
 
   main :: IO ()
   main = do
+    si_bf <- hGetBuffering stdin
+    print si_bf
+    so_bf <- hGetBuffering stdout
+    print so_bf
     putStrLn ": Answer one natural number."
     ans <- readLn :: IO Integer
     case 0 <= ans of
