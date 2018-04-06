0470f37fb73a0593f4ede728fb2d2deb21e444c2
Hexirp <Hexirp@users.noreply.github.com>
2018-04-06 21:45:24 +0900

    Delete `test`
    `test` is no longer necessary.

--- Game.hs
+++ Game.hs
@@ -15,28 +15,10 @@ module Game where
     ans <- readLn :: IO Integer
     putStrLn $ game ": You answered " "." ": You are a baby" ans
 
-    test
-
   game :: String -> String -> String -> Integer -> String
   game s0 s1 s2 x
     | 0 <= x    = s0 ++ show x ++ s1
     | otherwise = s2
-
-  test :: IO ()
-  test = do
-    print $ (maxBound                 :: Word64)
-    print $ ((2 ^ 64 - 1)             :: Word64)
-    print $ ((2 ^ 64 - 1) `div` 2     :: Word64)
-    print $ (shift (2 ^ 64 - 1) (-1)  :: Word64)
-    print $ (lShift (-1) (2 ^ 64 - 1) :: Word64)
-    print $ ((2 ^ 64 - 3)             :: Word64)
-    print $ ((2 ^ 64 - 3) `div` 2     :: Word64)
-    print $ (shift (2 ^ 64 - 3) (-1)  :: Word64)
-    print $ (lShift (-1) (2 ^ 64 - 3) :: Word64)
-    print $ ((2 ^ 64 - 3)             :: Word64)
-    print $ ((2 ^ 64 - 3) `div` 4     :: Word64)
-    print $ (shift (2 ^ 64 - 3) (-2)  :: Word64)
-    print $ (lShift (-2) (2 ^ 64 - 3) :: Word64)
 
   xorshift64 :: Word64 -> Word64
   xorshift64 = lShift 17 . lShift (-7) . lShift 13
