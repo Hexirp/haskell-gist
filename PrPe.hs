dc95c7631b08f19352dda32edf4941b924988f6e
Hexirp <Hexirp@users.noreply.github.com>
2018-07-19 18:35:43 +0900

    update

--- PrPe.hs
+++ PrPe.hs
@@ -6,9 +6,19 @@ module PrPe where
  escape ('\\' : xs) = '\\' : '\\' : escape xs
  escape (x    : xs) = x : escape xs
 
- lines_ :: String -> [String]
- lines_ = go id where
+ lines1 :: String -> [String]
+ lines1 = go id where
   go :: (String -> String) -> String -> [String]
   go f []          = f [] : []
   go f ('\n' : xs) = f [] : go id xs
   go f (x    : xs) = go (f . (x :)) xs
+ 
+ lines2 :: String -> [String]
+ lines2 []          = [] : []
+ lines2 ('\n' : xs) = [] : lines2 xs
+ lines2 (x    : xs) = lines2_f x (lines2 xs)
+
+ lines2_f :: Char -> [String] -> [String]
+  -- It's impossible case, but I have good answer.
+ lines2_f x []       = (x : []) : []
+ lines2_f x (y : ys) = (x : y) : ys
