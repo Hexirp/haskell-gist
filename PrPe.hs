703d188dbad177d81391bc8eff59cd81c3257a2e
Hexirp <Hexirp@users.noreply.github.com>
2018-07-19 18:25:19 +0900

    update

--- PrPe.hs
+++ PrPe.hs
@@ -1,8 +1,14 @@
 module PrPe where
  import Prelude
 
- -- | Escape \\.
  escape :: String -> String
  escape [] = []
  escape ('\\' : xs) = '\\' : '\\' : escape xs
  escape (x    : xs) = x : escape xs
+
+ lines_ :: String -> [String]
+ lines_ = go id where
+  go :: (String -> String) -> String -> [String]
+  go f []          = f [] : []
+  go f ('\n' : xs) = f [] : go id xs
+  go f (x    : xs) = go (f . (x :)) xs
