5022c1bfc625f4b4d4b19dee842d317e870ccc62
Hexirp <Hexirp@users.noreply.github.com>
2018-07-24 13:03:03 +0900

    fix

--- Stream.hs
+++ Stream.hs
@@ -16,5 +16,5 @@ module Stream where
  sCons :: a -> Streamly m a -> Streamly m a
  sCons x xs = Streamly $ \_ inCons -> inCons x xs
 
- sConsM :: Monad m => m a -> Streamly m a -> Sreamly m a
+ sConsM :: Monad m => m a -> Streamly m a -> Streamly m a
  sConsM mx xs = Streamly $ \_ inCons -> join $ inCons <$> mx <*> pure xs
