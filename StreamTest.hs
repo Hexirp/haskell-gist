bd5c34bf7fe043beee669c7dee771335f098eb90
Hexirp <Hexirp@users.noreply.github.com>
2018-07-25 14:26:24 +0900

    fix

--- StreamTest.hs
+++ StreamTest.hs
@@ -4,16 +4,16 @@ module StreamTest where
  import Stream
 
  s1 :: Stream IO ()
- s1 = print 1 |: print 2 |: iNil
+ s1 = print 1 |: print 2 |: sNil
 
  s2 :: Stream IO ()
- s2 = print 3 |: print 4 |: iNil
+ s2 = print 3 |: print 4 |: sNil
 
  s12 :: Stream IO ()
  s12 = s1 <> s2
 
  i1 :: Iteratee () IO Int
- i1 = iYield 1 $ iYield 2 $ iYield 3 iDone
+ i1 = iYield 1 $ iYield 2 iDone
 
  i2 :: Iteratee Int IO ()
  i2 = iAwait $ \a -> iYieldM (print a) $ iAwait $ \b -> iYieldM (print b) iDone
