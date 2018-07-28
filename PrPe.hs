dcc189236c07df6dd2b612e28a2a78ce288c937d
Hexirp <Hexirp@users.noreply.github.com>
2018-07-28 13:40:50 +0900

    update

--- PrPe.hs
+++ PrPe.hs
@@ -6,21 +6,11 @@ module PrPe where
  escape ('\\' : xs) = '\\' : '\\' : escape xs
  escape (x    : xs) = x : escape xs
 
- -- lines1 === lines2
+ -- lines1 === lines2 === lines4 === lines5 === lines6 === lines7 =\= lines3
  --
- -- lines1 "" = [""]
- -- lines2 "" = [""]
- -- lines3 "" = []
- --
- -- lines1 "\n" = ["",""]
- -- lines2 "\n" = ["",""]
- -- lines3 "\n" = [""]
- --
- -- lines1 "\n\n" = ["","",""]
- -- lines2 "\n\n" = ["","",""]
- -- lines3 "\n\n" = ["",""]
- --
- -- lines1 === lines4, but lines4 is wrong.
+ -- lines4 and lines5 causes a space leak. linesN is expected to have O(n)
+ -- performance ... n is the length of the string. But, lines4 and lines5 have
+ -- O(n^2) performance.
 
  lines1 :: String -> [String]
  lines1 = go id where
@@ -53,7 +43,7 @@ module PrPe where
   go :: String -> String -> [String]
   go acc []          = acc : []
   go acc ('\n' : xs) = acc : go [] xs
-  go acc (x    : xs) = go (acc ++ x : []) xs
+  go acc (x    : xs) = go (acc ++ [x]) xs
 
  lines5 :: String -> [String]
  lines5 = go [] where
@@ -62,7 +52,22 @@ module PrPe where
   go acc ('\n' : xs) = reverse acc : go [] xs
   go acc (x    : xs) = go (x : acc) xs
 
- -- 正格
+ lines6 :: String -> [String]
+ lines6 = uncurry (:) . go where
+  go :: String -> (String, [String])
+  go []          = ([], [])
+   -- Alternatively, ([], lines6 xs)
+  go ('\n' : xs) = let (y, ys) = go xs in ([], y : ys)
+  go (x    : xs) = let (y, ys) = go xs in (x : y, ys)
+
+ lines7 :: String -> [String]
+ lines7 = go (:) where
+  go :: (String -> [String] -> r) -> String -> r
+  go k []          = k [] []
+  go k ('\n' : xs) = go (\y ys -> k [] (y : ys)) xs
+  go k (x    : xs) = go (\y ys -> k (x : y) ys) xs
+
+ -- Strict test
  test1 :: (String -> [String]) -> Int -> IO ()
  test1 f n = let src = src1 n in print src >> print (f src)
 
