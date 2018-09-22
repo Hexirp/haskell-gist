41e24aa9f01ede1c4f19bb07e819f55d4423553b
Hexirp <Hexirp@users.noreply.github.com>
2018-09-22 12:23:01 +0900

    Update: 解像度を上げる

--- CC.hs
+++ CC.hs
@@ -9,8 +9,15 @@ module CC where
  main = do
   x <- defaultMain [
    bgroup "cham" [
-    bench "10000" $ nf cham 10000,
     bench "100000" $ nf cham 100000,
+    bench "200000" $ nf cham 200000,
+    bench "300000" $ nf cham 300000,
+    bench "400000" $ nf cham 400000,
+    bench "500000" $ nf cham 500000,
+    bench "600000" $ nf cham 600000,
+    bench "700000" $ nf cham 700000,
+    bench "800000" $ nf cham 800000,
+    bench "900000" $ nf cham 900000,
     bench "1000000" $ nf cham 1000000]
    ]
   return x
