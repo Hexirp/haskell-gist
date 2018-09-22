60040850fa0cf0de6e9f015fbf2ad45bd3d27222
Hexirp <Hexirp@users.noreply.github.com>
2018-09-22 17:23:42 +0900

    Update: 1/10

--- CC.hs
+++ CC.hs
@@ -9,25 +9,25 @@ module CC where
  main = do
   x <- defaultMain [
    bgroup "cham" [
+    bench "1000" $ nf cham 1000,
+    bench "2000" $ nf cham 2000,
+    bench "3000" $ nf cham 3000,
+    bench "4000" $ nf cham 4000,
+    bench "5000" $ nf cham 5000,
+    bench "6000" $ nf cham 6000,
+    bench "7000" $ nf cham 7000,
+    bench "8000" $ nf cham 8000,
+    bench "9000" $ nf cham 9000,
     bench "10000" $ nf cham 10000,
-    bench "20000" $ nf cham 20000,
-    bench "30000" $ nf cham 30000,
-    bench "40000" $ nf cham 40000,
-    bench "50000" $ nf cham 50000,
-    bench "60000" $ nf cham 60000,
-    bench "70000" $ nf cham 70000,
-    bench "80000" $ nf cham 80000,
-    bench "90000" $ nf cham 90000,
-    bench "100000" $ nf cham 100000,
-    bench "110000" $ nf cham 110000,
-    bench "120000" $ nf cham 120000,
-    bench "130000" $ nf cham 130000,
-    bench "140000" $ nf cham 140000,
-    bench "150000" $ nf cham 150000,
-    bench "160000" $ nf cham 160000,
-    bench "170000" $ nf cham 170000,
-    bench "180000" $ nf cham 180000,
-    bench "190000" $ nf cham 190000]
+    bench "11000" $ nf cham 11000,
+    bench "12000" $ nf cham 12000,
+    bench "13000" $ nf cham 13000,
+    bench "14000" $ nf cham 14000,
+    bench "15000" $ nf cham 15000,
+    bench "16000" $ nf cham 16000,
+    bench "17000" $ nf cham 17000,
+    bench "18000" $ nf cham 18000,
+    bench "19000" $ nf cham 19000]
    ]
   return x
 
