e8815e4a629198b890edd126ea8e90c7305cfc50
Hexirp <Hexirp@users.noreply.github.com>
2018-09-22 13:13:00 +0900

    Update: さらに上限を大きく

--- CC.hs
+++ CC.hs
@@ -18,7 +18,16 @@ module CC where
     bench "700000" $ nf cham 700000,
     bench "800000" $ nf cham 800000,
     bench "900000" $ nf cham 900000,
-    bench "1000000" $ nf cham 1000000]
+    bench "1000000" $ nf cham 1000000,
+    bench "1100000" $ nf cham 1100000,
+    bench "1200000" $ nf cham 1200000,
+    bench "1300000" $ nf cham 1300000,
+    bench "1400000" $ nf cham 1400000,
+    bench "1500000" $ nf cham 1500000,
+    bench "1600000" $ nf cham 1600000,
+    bench "1700000" $ nf cham 1700000,
+    bench "1800000" $ nf cham 1800000,
+    bench "1900000" $ nf cham 1900000]
    ]
   return x
 
