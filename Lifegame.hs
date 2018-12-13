9474d4b0ab4d12e325959c8a3a6677ca760437c0
Hexirp <Hexirp@users.noreply.github.com>
2018-12-13 21:16:04 +0900

    add

---
+++ Lifegame.hs
@@ -0,0 +1,28 @@
+module Lifegame where
+
+ import Prelude
+
+ main :: IO ()
+ main = return ()
+
+ type Array = Int -> Int -> Bool
+
+ -- ###
+ -- #
+ --  #
+ new :: Array
+ new = \x_ y_ -> let
+   x = x_ `mod` 80
+   y = y_ `mod` 80
+  in
+   if
+    False
+     || (x == 0 && y == 0)
+     || (x == 1 && y == 0)
+     || (x == 2 && y == 0)
+     || (x == 0 && y == 1)
+     || (x == 1 && y == 2)
+   then
+    True
+   else
+    False
