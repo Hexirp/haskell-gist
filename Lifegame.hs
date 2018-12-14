40c4d03ea6bfd171c72845f0dc6f95ee66813e0d
Hexirp <Hexirp@users.noreply.github.com>
2018-12-14 20:02:56 +0900

    Add: 道具

--- Lifegame.hs
+++ Lifegame.hs
@@ -21,3 +21,30 @@ module Lifegame where
     || (x == 2 && y == 0)
     || (x == 2 && y == 1)
     || (x == 2 && y == 2)
+
+ update :: Array -> Array
+ update a = \x y -> let
+  b = a x y
+  n = 0
+   + bti (a (x - 1) (y - 1))
+   + bti (a (x - 1) (y + 0))
+   + bti (a (x - 1) (y + 1))
+   + bti (a (x + 0) (y - 1))
+   + bti (a (x + 0) (y + 0))
+   + bti (a (x + 0) (y + 1))
+   + bti (a (x + 1) (y - 1))
+   + bti (a (x + 1) (y + 0))
+   + bti (a (x + 1) (y + 1))
+  in
+   next b n
+
+ next :: Bool -> Int -> Bool
+ next False 3 = True
+ next False _ = False
+ next True  2 = True
+ next True  3 = True
+ next True  _ = False
+
+ bti :: Bool -> Int
+ bti False = 0
+ bti True = 1
