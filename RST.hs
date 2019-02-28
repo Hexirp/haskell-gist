3413e02e976985bd827c9772bde4244efe4807f2
Hexirp <Hexirp@users.noreply.github.com>
2019-02-28 20:37:44 +0900

    Update: reStructureText っぽいのを作ってみる
    pandoc を blog で使っていて色々不満が出てきたので。

---
+++ RST.hs
@@ -0,0 +1,8 @@
+module RST where
+
+ line :: String -> [String]
+ line = go id where
+  go :: (String -> String) -> String -> [String]
+  go f []          = f [] : []
+  go f ('\n' : xs) = f [] : go id xs
+  go f (x    : xs) = go (f . (x :)) xs
