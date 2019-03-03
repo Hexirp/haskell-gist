cdf1b963c5f0d310602815c0a86b8e8c2d6d582b
Hexirp <Hexirp@users.noreply.github.com>
2019-03-03 10:14:46 +0900

    Add: title2_line

--- RST.hs
+++ RST.hs
@@ -18,10 +18,29 @@ module RST where
  title yes no x = case x of
   [t, l]    -> title2 yes no t l
   [l, t, r] -> title3 yes no l t r
-  _         -> no x
+  x'        -> no x'
 
  title2 :: a -> ([String] -> a) -> String -> String -> a
- title2 yes no t l = undefined
+ title2 yes no t l = case t of
+  [] -> no [[], l]
+  t' -> title2_line yes no t' l
+
+ title2_line :: a -> ([String] -> a) -> String -> String -> a
+ title2_line yes no t l = case l of
+  '=' : _ -> undefined
+  '-' : _ -> undefined
+  '`' : _ -> undefined
+  ':' : _ -> undefined
+  ''' : _ -> undefined
+  '"' : _ -> undefined
+  '~' : _ -> undefined
+  '^' : _ -> undefined
+  '_' : _ -> undefined
+  '*' : _ -> undefined
+  '+' : _ -> undefined
+  '#' : _ -> undefined
+  '<' : _ -> undefined
+  '>' : _ -> undefined
 
  title3 :: a -> ([String] -> a) -> String -> String -> String -> a
  title3 yes no l t r = undefined
