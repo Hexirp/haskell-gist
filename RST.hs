e74174d720c981db170ed28ae781e9938efd0129
Hexirp <Hexirp@users.noreply.github.com>
2019-03-03 10:04:25 +0900

    Add: title, title2, and title3

--- RST.hs
+++ RST.hs
@@ -13,3 +13,15 @@ module RST where
   go f []        = f [] : []
   go f ("" : xs) = f [] : go id xs
   go f (x  : xs) = go (f . (x :)) xs
+
+ title :: a -> ([String] -> a) -> [String] -> a
+ title yes no x = case x of
+  [t, l]    -> title2 yes no t l
+  [l, t, r] -> title3 yes no l t r
+  _         -> no x
+
+ title2 :: a -> ([String] -> a) -> String -> String -> a
+ title2 yes no t l = undefined
+
+ title3 :: a -> ([String] -> a) -> String -> String -> String -> a
+ title3 yes no l t r = undefined
