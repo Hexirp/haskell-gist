8ae84e59db78ecc18282c7a91bdb1ca03d0dea2a
Hexirp <Hexirp@users.noreply.github.com>
2019-03-20 14:20:43 +0900

    Update: Conduit.hs

--- Conduit.hs
+++ Conduit.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE GADTs #-}
+
 -- iteratee, conduit, ... などのストリーム処理ライブラリは [String] -> [Int] と
 -- いうようなリストからリストへの関数をよりよく表すものである。このような関数は
 -- 多くの場合、遅延評価によって省スペースで処理できる。しかし、この性質は簡単に
@@ -6,7 +9,7 @@
 
 map' :: (a -> b) -> [a] -> [b]
 map' f = go where
- go x = case x of      -- await
+ go x = case x of      --   await
   []     -> []         --   done
   x : xs -> f x : go x --   yield & continue
 
@@ -14,15 +17,32 @@ map' f = go where
 -- ている。ちなみに、これはパターンマッチングによる評価の強制により駆動するので
 -- pull 式ということになるのだと思う。
 
-data Str a b = Nil b | Cons a (Str a b)
+data Fix f where
+ Fix :: forall x. (x -> Fix f) -> f x -> Fix f
+
+data StrF a b i = Nil b | Cons a i
+
+type Str a b = Fix (StrF a b)
 
 -- このようなデータ構造を考える。ここでリストを使わないのは終端処理で値を返した
 -- い場合があるからだ。つまりリストでは return を実現できない。
 
-data Vessel i o u r
- = Yield o (Vessel i o u r)
- | Await (i -> Vessel i o u r) (u -> Vessel i o u r)
- | Done r
+data Vessel' i o u r
+ = Yield' o (Vessel' i o u r)
+ | Await' (i -> Vessel' i o u r) (u -> Vessel' i o u r)
+ | Done' r
 
 -- これが conduit の組み立て方である。これは Free や Coyoneda を型レベルで展開
--- しているので分かりにくい。
+-- しているので分かりにくい。Str を援用して分かりやすくする。
+
+newtype Freer f a = Freer {
+  runFreer :: forall m. Monad m => (forall t. f t -> m t) -> m a }
+
+data VesselF i o u r where
+ Await :: VesselF i o u (StrF a b ())
+ Yield :: o -> VesselF i o u ()
+
+type Vessel i o u = Freer (VesselF i o u)
+
+runVessel :: Vessel i o u r -> Str i u -> Str o r
+runVessel (Freer k) 
