09d6984fd178acb754734508c09731223d8e0f9c
Hexirp <Hexirp@users.noreply.github.com>
2019-03-23 11:45:51 +0900

    Add: VesselM

--- Conduit.hs
+++ Conduit.hs
@@ -31,7 +31,7 @@ data Vessel i o u r
  | Await (i -> Vessel i o u r) (u -> Vessel i o u r)
 
 -- runVessel a b は a の実行結果に従い b から要素を取り出していく。
-runVessel :: Vessel i o u r -> Str i u -> Str o r
+runVessel :: forall i o u r. Vessel i o u r -> Str i u -> Str o r
 runVessel = goR where
  goR :: Vessel i o u r -> Str i u -> Str o r
  goR s t = case s of
@@ -46,12 +46,17 @@ runVessel = goR where
 -- fuse a b は a の実行結果に従い b から要素を取り出していく。
 fuse :: forall a b c d e f. Vessel b e d f -> Vessel a b c d -> Vessel a e c f
 fuse = goR where
+
  goR :: Vessel b e d f -> Vessel a b c d -> Vessel a e c f
  goR s t = case s of
   Done sr -> Done sr
   Yield so sk -> Yield so (goR sk t)
   Await se sf -> goL se sf t
- goL :: (b -> Vessel b e d f) -> (d -> Vessel b e d f) -> Vessel a b c d -> Vessel a e c f
+
+ goL :: (b -> Vessel b e d f)
+     -> (d -> Vessel b e d f)
+     -> Vessel a b c d
+     -> Vessel a e c f
  goL se sf t = case t of
   Done tr -> goR (sf tr) (Done tr)
   Yield to tk -> goR (se to) tk
@@ -73,7 +78,12 @@ strToVessel (Cons x xs) = Yield x (strToVessel xs)
 
 -- 以下のような変性をもつ。
 
-mapVessel :: (i' -> i) -> (o -> o') -> (u' -> u) -> (r -> r') -> Vessel i o u r -> Vessel i' o' u' r'
+mapVessel :: (i' -> i)
+          -> (o -> o')
+          -> (u' -> u)
+          -> (r -> r')
+          -> Vessel i o u r
+          -> Vessel i' o' u' r'
 mapVessel fi fo fu fr = go where
  go (Done r) = Done (fr r)
  go (Yield o k) = Yield (fo o) (go k)
@@ -90,10 +100,14 @@ joinVessel s = case s of
  Yield so sk -> Yield so (joinVessel sk)
  Await se sf -> Await (\i -> joinVessel (se i)) (\u -> joinVessel (sf u))
 
-bindVessel :: Vessel x0 x1 x2 a -> (a -> Vessel x0 x1 x2 b) -> Vessel x0 x1 x2 b
+bindVessel :: Vessel x0 x1 x2 a
+           -> (a -> Vessel x0 x1 x2 b)
+           -> Vessel x0 x1 x2 b
 bindVessel x f = joinVessel (mapVessel id id id f x)
 
-fappVessel :: Vessel x0 x1 x2 (a -> b) -> Vessel x0 x1 x2 a -> Vessel x0 x1 x2 b
+fappVessel :: Vessel x0 x1 x2 (a -> b)
+           -> Vessel x0 x1 x2 a
+           -> Vessel x0 x1 x2 b
 fappVessel f x = f `bindVessel` \f' -> mapVessel id id id f' x
 
 -- 型クラスのインスタンス。
@@ -107,6 +121,38 @@ instance Applicative (Vessel x0 x1 x2) where
 
 instance Monad (Vessel x0 x1 x2) where
  (>>=) = bindVessel
+
+-- 変換子チックな。
+
+data StrM m a b = NilM (m b) | ConsM (m (a, StrM m a b))
+
+data VesselM m n i o u r
+ = DoneM (n r)
+ | YieldM (n (o, VesselM m n i o u r))
+ | AwaitM (m i -> VesselM m n i o u r) (m u -> VesselM m n i o u r)
+
+runVesselM :: forall m n i o u r.
+              (Monad m, Monad n)
+           => VesselM m n i o u r
+           -> StrM m i u
+           -> StrM n o r
+runVesselM = goR where
+
+ goR :: VesselM m n i o u r -> StrM m i u -> StrM n o r
+ goR s t = case s of
+  DoneM nsr -> NilM nsr
+  YieldM nsok -> ConsM (nsok >>= \(so, sk) -> (so, goR sk t))
+  AwaitM se sf -> goL se sf t
+
+ goL :: (m i -> VesselM m n i o u r)
+     -> (m u -> VesselM m n i o u r)
+     -> StrM m i u
+     -> StrM n o r
+ goL se sf t = case t of
+  NilM mtr -> goR (sf mtr) (Nil tr)
+  ConsM mtok -> mtok >>= \(to, tk) -> goR (se to) tk
+
+-- ダミー。
 
 main :: IO ()
 main = return ()
