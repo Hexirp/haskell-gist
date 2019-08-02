fcb67ff82edbfdbfe0f5ae01e64391e9d8c70675
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-08-02 22:25:37 +0900

    Add: paragraphs

--- Midare.hs
+++ Midare.hs
@@ -20,3 +20,24 @@ module Midare where
     go xv (xsv : xss) = case xv of
       [] -> '\n' : go xsv xss
       xvv : xvs -> xvv : go xvs (xsv : xss)
+
+  -- lines . unlines
+
+  unparagraphs :: [String] -> [[String]]
+  unparagraphs x = go id x
+   where
+    go :: ([[String]] -> [[String]]) -> [String] -> [[String]]
+    go s [] = s [] : []
+    go s (xv : xs) = case xv of
+      [] -> s [] : go id xs
+      _ -> go (\y -> s (xv : y)) xs
+
+  paragraphs :: [[String]] -> [String]
+  paragraphs [] = []
+  paragraphs (xv : xs) = go xv xs
+   where
+    go :: [String] -> [[String]] -> [String]
+    go xv [] = xv
+    go xv (xsv : xss) = case xv of
+      [] -> [] : go xsv xss
+      xvv : xvs -> xvv : go xvs (xsv : xss)
