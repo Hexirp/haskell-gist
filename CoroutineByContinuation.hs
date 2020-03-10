4bddb88e0e41fb9c4880c9aa9398c7de2ecf437e
Hexirp <Hexirp@users.noreply.github.com>
2020-03-10 13:44:03 +0900

    Fix:

--- CoroutineByContinuation.hs
+++ CoroutineByContinuation.hs
@@ -125,5 +125,5 @@ module Main where
   yield_1 :: a -> IORef (Source (Cio r r) a) -> Cio (Source (Cio _ _) _) (Source (Cio _ _) _) ()
   yield_1 x ref = Cio $ \k -> _ (runCio (yield_0 x ref) $ \y -> _ (k y))
     where
-      des :: Source (Cio r r) a -> Step a (Source (Cio r r) a)
+      deS :: Source (Cio r r) a -> Step a (Source (Cio r r) a)
       deS x = _ (unSource x)
